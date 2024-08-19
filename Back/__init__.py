9fa9f4cb1140878030df925cf8f762381be5df20
EdiPAIN <edige_musa@mail.ru>
2024-08-19 16:06:26 +0600

    [UPDATE] CRUD application

---
+++ Back/__init__.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from Back.models import Base
+from Back.database import engine
+
+app = FastAPI()
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+@app.on_event("startup")
+async def startup_event():
+    await init_db()
